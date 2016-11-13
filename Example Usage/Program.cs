bad747cb524d2d33f956e7cd0fa8619e3f4b35a4
hushoca <hushoca@yahoo.com>
2016-11-13 22:57:29 +0000

    Uploading Version 1.0
    Uploading the first version of the TextReport DLL.

---
+++ Example Usage/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using TextReport;
+
+namespace DLLTestApplication
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            //2D array
+            String[,] table = {
+                { "Title", "Author", "Version", "Date" },
+                {"A walk in the woods","George Woodland","1.0","12 Dec 2014" },
+                {"A death wish","Ricky Martin","2.6","01 Jan 1991" }
+            };
+
+            //create a new table with 6 rows and 3 columns
+            Table t = new Table(6, 3);
+
+            t.addRow(new String[]{"Name","Surname","Date of Birth"});
+            t.addRow(new String[] {"George", "Belkow", "01/08/1925" });
+
+            //will be ignored because its larger then its supposed to be
+            t.addRow(new String[] {"Emma", "Sparrow", "08/09/2011","Test" }); 
+            t.addRow(new String[] { "Ryan", "Apple","25/02/1881" }); 
+
+            //create a report with name text
+            //will be saved as executable_directory\Test.txt
+            TXTReport rep = new TXTReport("Test");
+
+            //set character limit to 80
+            rep.setLineCharacterLimit(80);
+
+            //add text
+            rep.appendText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie orci justo, vel cursus tortor mattis vel. Praesent scelerisque est urna, ac ullamcorper turpis rhoncus nec. Etiam dapibus sollicitudin orci, sit amet porttitor ex varius at. Nulla at felis mi. Maecenas interdum lorem risus, non dapibus justo cursus quis. Donec quis dui sollicitudin, maximus turpis sed, efficitur erat. Integer feugiat varius sollicitudin. Fusce laoreet, diam quis placerat iaculis, arcu sapien feugiat mi, quis volutpat nunc diam ac lorem. Quisque suscipit erat vel nulla posuere aliquam. Nullam auctor, felis vel feugiat sollicitudin, augue lacus convallis orci, sed interdum magna augue id diam. Pellentesque efficitur turpis pharetra, fringilla nulla ac, sagittis dolor. Nullam bibendum ultrices molestie. Donec hendrerit turpis nec rhoncus rutrum.");
+
+            //start a new line
+            rep.breakLine();
+
+            //add the table from 2D array
+            rep.addTable(table,1);
+
+            //start a new line
+            rep.breakLine();
+
+            //add text
+            rep.appendText("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie orci justo, vel cursus tortor mattis vel. Praesent scelerisque est urna, ac ullamcorper turpis rhoncus nec. Etiam dapibus sollicitudin orci, sit amet porttitor ex varius at. Nulla at felis mi. Maecenas interdum lorem risus, non dapibus justo cursus quis. Donec quis dui sollicitudin, maximus turpis sed, efficitur erat. Integer feugiat varius sollicitudin. Fusce laoreet, diam quis placerat iaculis, arcu sapien feugiat mi, quis volutpat nunc diam ac lorem. Quisque suscipit erat vel nulla posuere aliquam. Nullam auctor, felis vel feugiat sollicitudin, augue lacus convallis orci, sed interdum magna augue id diam. Pellentesque efficitur turpis pharetra, fringilla nulla ac, sagittis dolor. Nullam bibendum ultrices molestie. Donec hendrerit turpis nec rhoncus rutrum.");
+            
+            //add table from Table object
+            rep.addTable(t,8);
+
+            //save the file and print the returned value (null if failed
+            //and file path if success.
+            Console.WriteLine(rep.save());
+
+            while (true) { }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# TextReportDLL
-A dll which produces simple text files (reports) with few lines of code.These reports can contain tables and strings but the main feature of the library is it's simplicity.
+#TextReport DLL#
+----------
+
+## About ##
+This library is created so that I can get fast report files from my programs and I am open-sourcing it because I love to share. Its written in a way that with few lines of code you can get tables, text and nice formatting features in the report. I will mainly use it in small projects where I need logging over a time period.
+
+## Documentation ##
+For all the classes and methods please check the help file which is under the Documentation folder. Tried to make the help file as clear and simple as possible also I have an example under the examples folder.
+
+## Features ##
+
+> - Can create a text file by choosing the path and name. Or just a file name (it will create the file in the executable folder)
+> - Can create tables from 2D string arrays or by using the Table class built-in.
+> - Can be used by beginners because you can easily appendText() or breakLine()
+> - Saving returns the path of the created file which makes it easy to open
+> - Can create a report with only few lines of code.
+> - Well documented.
+
+## Changelog ##
+Still in version 1.0
+
+![](https://cdn.meme.am/instances/500x/67126131.jpg)
